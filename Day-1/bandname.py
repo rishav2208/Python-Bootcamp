a54c6242c4c118b4cdcc8339979098ccacce18e2
Rishav Kumar <32488994+rishav2208@users.noreply.github.com>
2021-06-18 01:14:55 +0530

    Add files via upload

---
+++ Day-1/bandname.py
@@ -0,0 +1,11 @@
+#Simple py script using string concatenation
+
+print("WELCOME to Band name generator")
+
+city=input('Which city you grew up in?\n')
+
+pet= input('What is the name of your pet?\n')
+
+bandname=pet+ " "+ pet+ " "+city
+
+print('Your awesome band name is:',bandname)
